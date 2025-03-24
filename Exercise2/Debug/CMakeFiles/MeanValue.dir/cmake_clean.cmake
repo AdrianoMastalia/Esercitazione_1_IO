51067e5a6ee7735b188038ee0fb7d6b54ff1bf9c
Adriano <s308388@studenti.polito.it>
2025-03-24 10:00:48 +0100

    Caricamento cartella in cui è presente l'output

---
+++ Exercise2/Debug/CMakeFiles/MeanValue.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeanValue.dir/main.cpp.o"
+  "CMakeFiles/MeanValue.dir/main.cpp.o.d"
+  "CMakeFiles/MeanValue.dir/map_point.cpp.o"
+  "CMakeFiles/MeanValue.dir/map_point.cpp.o.d"
+  "MeanValue"
+  "MeanValue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MeanValue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
